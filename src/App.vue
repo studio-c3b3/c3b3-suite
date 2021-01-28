e887feab7229e9cb6c76322fa1a47c5496620a86
GnomeApprenti <lchakroun@outlook.fr>
2021-01-27 19:57:46 -0600

    Ajout d'effets hover avec trasitions smooth

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
+
   <div id="header">
-    <h1 id="titre">C3B3</h1>
-    <div id="nav">
-        <router-link to="/" id="pages">Home</router-link> |
-        <router-link to="/films" id="pages">Films</router-link>
+
+    <div id="logo">
+      <h1 id="titre">C3B3</h1>
     </div>
+
+    <div id="slogan-div">
+      <h3 id="slogan">j'ai les données, j'ai les datas</h3>
+    </div>
+
+    <div id="nav">
+
+        <div id="home">
+          <router-link to="/" id="pages">Home</router-link> 
+        </div>
+
+        <div id ="films">
+          <router-link to="/films" id="pages">Films</router-link>
+        </div>
+        
+    </div>
+
   </div>
+
   <router-view />
 </template>
