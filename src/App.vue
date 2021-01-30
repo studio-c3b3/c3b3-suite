7455b45d8e9a3516f6043640c0860087401465b4
GnomeApprenti <lchakroun@outlook.fr>
2021-01-29 18:35:09 -0600

    Ajout du système d'upload basique

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,6 @@
       <h1 id="titre">C3B3</h1>
     </div>
 
-    <div id="slogan-div">
-      <h3 id="slogan">j'ai les données, j'ai les datas</h3>
-    </div>
-
     <div id="nav">
 
         <div id="home">
---
+++ src/views/SimpleUpload.vue
@@ -0,0 +1,32 @@
+<template>
+  <form enctype="multipart/form-data">
+
+    <div class="field">
+      <label for="file" class="label">Upload les diez </label>
+      <input
+        type="file"
+        ref="file"
+        @change="selectFile"
+      />
+    </div>
+
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SimpleUpload",
+
+  data(){
+    return{
+      file: ""
+    }
+  },
+
+  methods: {
+    selectFile(){
+      this.file = this.$refs.file.files[0]
+    }
+  }
+}
+</script>
