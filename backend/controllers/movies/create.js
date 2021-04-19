a565eb90e7bde989d143db5577de4634c00653c1
Faustin M <faustino40@outlook.fr>
2021-04-19 21:17:03 +0200

    Ajout de la vérification du type et modification des paramètres

--- backend/controllers/movies/create.js
+++ backend/controllers/movies/create.js
@@ -1,10 +1,18 @@
-module.exports = (req, res) => {
+const uuid = require("uuid")
+
+module.exports = async (req, res) => {
     console.log(req.files)
-    if (!req.files) {
+    if (!req.files || !req.files.file) {
       return res.status(400).send('No files were uploaded.');
     }
     console.log(req.files)
+    if (!req.files.file.mimetype.startsWith('video/')){
+      return res.status(400).send('Invalid format')
+    }
 
+    uuid.v4()
+    const uploadPath = `${__dirname}/../../../movies/` + req.files.file.name;
+    await req.files.file.mv(uploadPath)
 
     return res.send('Fine !')
 }
--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,13 @@ const port = 3000;
 const cors = require("cors");
 const fileUpload = require('express-fileupload');
 
-app.use(fileUpload())
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/',
+    createParentPath: true,
+    abortOnLimit: true,
+    limits: { fileSize: 5 * 1000 * 1024 * 1024 }, // Limitation des fichiers à 5Go
+}))
 app.use(cors());
 
 const moviesRouter = require('./routes/movies')
